267c17c9139f637f754cca6d7861e7959b647a11
meghasyam1997 <meghasyam01@gmail.com>
2025-02-18 23:32:01 +0530

    rabbitmq highlight Scripting

--- rabbitMQ.sh
+++ rabbitMQ.sh
@@ -1,7 +1,13 @@
-curl -s https://packagecloud.io/install/repositories/rabbitmq/erlang/script.rpm.sh | bash
-curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | bash
+echo -e "\e[35m>>>>>>>>>> Copy Repo File <<<<<<<<<<\e[0m"
+cp /home/ec2-user/Roboshop-shell/rabbitmq.repo /etc/yum.repos.d/rabbitmq.repo
+
+echo -e "\e[35m>>>>>>>>>> Install RabbitMQ Server <<<<<<<<<<\e[0m"
 dnf install rabbitmq-server -y
+
+echo -e "\e[35m>>>>>>>>>> Start RabbitMQ Service <<<<<<<<<<\e[0m"
 systemctl enable rabbitmq-server
 systemctl start rabbitmq-server
+
+echo -e "\e[35m>>>>>>>>>> Create one user for the application <<<<<<<<<<\e[0m"
 rabbitmqctl add_user roboshop roboshop123
 rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*"
