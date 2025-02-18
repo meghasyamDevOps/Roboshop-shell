2113988f39314843c45cc87c15815fd7891851a1
meghasyam1997 <meghasyam01@gmail.com>
2025-02-18 19:46:35 +0530

    Mongodb highlight script

--- mongodb.sh
+++ mongodb.sh
@@ -1,6 +1,15 @@
-cp mongo.repo /etc/yum.repos.d/mongo.repo
+echo -e "\e[36m>>>>>>>>>> Copy Mongo Repo File <<<<<<<<<<\e[0m"
+cp /home/ec2-user/Roboshop-shell/mongo.repo /etc/yum.repos.d/mongo.repo
+
+echo -e "\e[36m>>>>>>>>>> Install MonogoDB <<<<<<<<<<\e[0m"
 dnf install mongodb-org -y
+
+echo -e "\e[36m>>>>>>>>>> Enable Mongodb <<<<<<<<<<\e[0m"
 systemctl enable mongod
 systemctl start mongod
-# Update listen address from 127.0.0.1 to 0.0.0.0 in /etc/mongod.conf
+
+echo -e "\e[36m>>>>>>>>>> Chage the mongodb Listen Address <<<<<<<<<<\e[0m"
+sed -i -e 's|127.0.0.1|0.0.0.0|' /etc/mongod.conf
+
+echo -e "\e[36m>>>>>>>>>> Restart Mongodb <<<<<<<<<<\e[0m"
 systemctl restart mongod
