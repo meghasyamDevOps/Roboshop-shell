9122ac5265592b705712b02d4a5b87b78a74abed
meghasyam1997 <meghasyam01@gmail.com>
2025-02-18 19:23:39 +0530

    frontend highlight script

--- frontend.sh
+++ frontend.sh
@@ -1,9 +1,20 @@
+echo -e "\e[36m>>>>>>>>>> Install nignx<<<<<<<<<<\e[0m"
 dnf install nginx -y
+
+echo -e "\e[36m>>>>>>>>>> Enable nignx<<<<<<<<<<\e[0m"
 systemctl enable nginx
 systemctl start nginx
-# cp roboshop.conf /etc/nginx/default.d/roboshop.conf
+
+echo -e "\e[36m>>>>>>>>>> Copy Roboshop Config File<<<<<<<<<<\e[0m"
+cp /home/centos/Roboshop-shell/roboshop.conf /etc/nginx/default.d/roboshop.conf
+
+echo -e "\e[36m>>>>>>>>>> Download frontend Artifact<<<<<<<<<<\e[0m"
 rm -rf /usr/share/nginx/html/*
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
+
+echo -e "\e[36m>>>>>>>>>> Unzip the Artifact<<<<<<<<<<\e[0m"
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip
+
+echo -e "\e[36m>>>>>>>>>> Restart nignx<<<<<<<<<<\e[0m"
 systemctl restart nginx
