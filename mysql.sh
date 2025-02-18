fcc3a7f6199a63b9fd26c237c7e174efb7095d7a
meghasyam1997 <meghasyam01@gmail.com>
2025-02-18 23:08:55 +0530

    mysql highlight Scripting

--- mysql.sh
+++ mysql.sh
@@ -1,7 +1,9 @@
-dnf module disable mysql -y
-cp mysql.repo /etc/yum.repos.d/mysql.repo
-dnf install mysql-community-server -y
+echo -e "\e[35m>>>>>>>>>> Install MySQL Servere <<<<<<<<<<\e[0m"
+dnf install mysql-server -y
+
+echo -e "\e[35m>>>>>>>>>> Start MySQL Service <<<<<<<<<<\e[0m"
 systemctl enable mysqld
 systemctl start mysqld
+
+echo -e "\e[35m>>>>>>>>>> Change the default root password <<<<<<<<<<\e[0m"
 mysql_secure_installation --set-root-pass RoboShop@1
-mysql -u root -p RoboShop@1
