1a3a3b129fca99284b11184beba9eef5d0971b58
meghasyam1997 <meghasyam01@gmail.com>
2025-02-19 00:12:24 +0530

    Frontend highlight scripting

--- frontend.sh
+++ frontend.sh
@@ -1,20 +1,24 @@
-echo -e "\e[36m>>>>>>>>>> Install nignx<<<<<<<<<<\e[0m"
+echo -e "\e[35m>>>>>>>>>> Install Nginx <<<<<<<<<<\e[0m"
+dnf module disable nginx -y
+dnf module enable nginx:1.24 -y
 dnf install nginx -y
 
-echo -e "\e[36m>>>>>>>>>> Enable nignx<<<<<<<<<<\e[0m"
+echo -e "\e[35m>>>>>>>>>> Start & Enable Nginx service <<<<<<<<<<\e[0m"
 systemctl enable nginx
 systemctl start nginx
 
-echo -e "\e[36m>>>>>>>>>> Copy Roboshop Config File<<<<<<<<<<\e[0m"
-cp /home/ec2-user/Roboshop-shell/roboshop.conf /etc/nginx/default.d/roboshop.conf
+echo -e "\e[35m>>>>>>>>>> Copy the service File <<<<<<<<<<\e[0m"
+cp /home/ec2-user/Roboshop-shell/nginx.conf /etc/nginx/nginx.conf
 
-echo -e "\e[36m>>>>>>>>>> Download frontend Artifact<<<<<<<<<<\e[0m"
+echo -e "\e[35m>>>>>>>>>> Remove the default content  <<<<<<<<<<\e[0m"
 rm -rf /usr/share/nginx/html/*
-curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
 
-echo -e "\e[36m>>>>>>>>>> Unzip the Artifact<<<<<<<<<<\e[0m"
+echo -e "\e[35m>>>>>>>>>> Download the frontend content <<<<<<<<<<\e[0m"
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend-v3.zip
+
+echo -e "\e[35m>>>>>>>>>> Extract the frontend content <<<<<<<<<<\e[0m"
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip
 
-echo -e "\e[36m>>>>>>>>>> Restart nignx<<<<<<<<<<\e[0m"
+echo -e "\e[35m>>>>>>>>>> Restart Nginx Service <<<<<<<<<<\e[0m"
 systemctl restart nginx
