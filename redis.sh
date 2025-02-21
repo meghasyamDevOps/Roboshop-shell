516194930156a11d115405d2b55fff0fff194926
meghasyam1997 <meghasyam01@gmail.com>
2025-02-21 15:26:46 +0530

    making the on Script changes

--- redis.sh
+++ redis.sh
@@ -1,13 +1,13 @@
-echo -e "\e[36m>>>>>>>>>> download the dependencies <<<<<<<<<<\e[0m"
+echo -e "\e[36m>>>>>>>>>> Install redis <<<<<<<<<<\e[0m"
 dnf module disable redis -y
 dnf module enable redis:7 -y
 
-echo -e "\e[36m>>>>>>>>>> download the dependencies <<<<<<<<<<\e[0m"
+echo -e "\e[36m>>>>>>>>>> Download the dependencies <<<<<<<<<<\e[0m"
 dnf install redis -y
 
 echo -e "\e[36m>>>>>>>>>> Update listen address <<<<<<<<<<\e[0m"
 sed -i -e 's|127.0.0.1|0.0.0.0|' /etc/redis/redis.conf
-sed -i 's|protected-mode yes|protected-mode no|' /etc/redis/redis.conf
+sed -i -e 's|protected-mode yes|protected-mode no|' /etc/redis/redis.conf
 
 echo -e "\e[36m>>>>>>>>>> download the dependencies <<<<<<<<<<\e[0m"
 systemctl enable redis
