996b78993825e023aee437c8c4e274ae9e7b648b
meghasyam1997 <meghasyam01@gmail.com>
2025-02-21 16:40:36 +0530

    code DRY

--- common.sh
+++ common.sh
@@ -212,3 +212,26 @@ func_java() {
 
   func_schema_setup
 }
+
+func_mongodb() {
+  print_head "Copy Mongo Repo File"
+  cp ${script_path}/mongo.repo /etc/yum.repos.d/mongo.repo &>>$log_file
+  func_status_check $?
+
+  print_head "Install MonogoDB"
+  dnf install mongodb-org -y &>>$log_file
+  func_status_check $?
+
+  print_head "Enable Mongodb"
+  systemctl enable mongod &>>$log_file
+  systemctl start mongod &>>$log_file
+  func_status_check $?
+
+  print_head "Chage the mongodb Listen Address"
+  sed -i -e 's|127.0.0.1|0.0.0.0|' /etc/mongod.conf &>>$log_file
+  func_status_check $?
+
+  print_head "Restart Mongodb"
+  systemctl restart mongod &>>$log_file
+  func_status_check $?
+}
