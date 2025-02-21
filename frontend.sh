a8bcb73e00b0c1e83bde6106ebc716a3fc6ba82b
meghasyam1997 <meghasyam01@gmail.com>
2025-02-21 14:50:44 +0530

    Making code DRY

--- frontend.sh
+++ frontend.sh
@@ -1,24 +1,7 @@
-echo -e "\e[35m>>>>>>>>>> Install Nginx <<<<<<<<<<\e[0m"
-dnf module disable nginx -y
-dnf module enable nginx:1.24 -y
-dnf install nginx -y
+script=$(realpath "$0")
+script_path=$(dirname "$script")
+source ${script_path}/common.sh
 
-echo -e "\e[35m>>>>>>>>>> Start & Enable Nginx service <<<<<<<<<<\e[0m"
-systemctl enable nginx
-systemctl start nginx
+component=frontend
 
-echo -e "\e[35m>>>>>>>>>> Copy the service File <<<<<<<<<<\e[0m"
-cp /home/ec2-user/Roboshop-shell/nginx.conf /etc/nginx/nginx.conf
-
-echo -e "\e[35m>>>>>>>>>> Remove the default content  <<<<<<<<<<\e[0m"
-rm -rf /usr/share/nginx/html/*
-
-echo -e "\e[35m>>>>>>>>>> Download the frontend content <<<<<<<<<<\e[0m"
-curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend-v3.zip
-
-echo -e "\e[35m>>>>>>>>>> Extract the frontend content <<<<<<<<<<\e[0m"
-cd /usr/share/nginx/html
-unzip /tmp/frontend.zip
-
-echo -e "\e[35m>>>>>>>>>> Restart Nginx Service <<<<<<<<<<\e[0m"
-systemctl restart nginx
+func_nginx
