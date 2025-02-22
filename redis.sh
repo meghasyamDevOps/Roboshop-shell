33842cb271814341ffb24d7358082aca5802d79a
meghasyam1997 <meghasyam01@gmail.com>
2025-02-22 12:58:47 +0530

    Redis changes

--- redis.sh
+++ redis.sh
@@ -1,14 +1,25 @@
-echo -e "\e[36m>>>>>>>>>> Install redis <<<<<<<<<<\e[0m"
-dnf module disable redis -y
-dnf module enable redis:7 -y
+script=$(realpath "$0")
+script_path=$(dirname "$script")
+source ${script_path}/common.sh
 
-echo -e "\e[36m>>>>>>>>>> Download the dependencies <<<<<<<<<<\e[0m"
-dnf install redis -y
+print_head "Disable previous version redis"
+dnf module disable redis -y &>>$log_file
+func_status_check $?
 
-echo -e "\e[36m>>>>>>>>>> Update listen address <<<<<<<<<<\e[0m"
-sed -i -e 's|127.0.0.1|0.0.0.0|' /etc/redis/redis.conf
-sed -i -e 's|protected-mode yes|protected-mode no|' /etc/redis/redis.conf
+print_head "Enable redis"
+dnf module enable redis:7 -y &>>$log_file
+func_status_check $?
 
-echo -e "\e[36m>>>>>>>>>> download the dependencies <<<<<<<<<<\e[0m"
-systemctl enable redis
-systemctl restart redis
+print_head "Install redis"
+dnf install redis -y &>>$log_file
+func_status_check $?
+
+print_head "Update listen address"
+sed -i -e 's|127.0.0.1|0.0.0.0|' /etc/redis/redis.conf &>>$log_file
+sed -i -e 's|protected-mode yes|protected-mode no|' /etc/redis/redis.conf &>>$log_file
+func_status_check $?
+
+print_head "Enable & Restart redis"
+systemctl enable redis &>>$log_file
+systemctl restart redis &>>$log_file
+func_status_check $?
