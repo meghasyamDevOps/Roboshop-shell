773dfa55472c71cf1d6835f3c5e130594ec6a2c4
meghasyam1997 <meghasyam01@gmail.com>
2025-02-22 13:07:55 +0530

    shipping changes

--- shipping.sh
+++ shipping.sh
@@ -1,8 +1,13 @@
 script=$(realpath "$0")
 script_path=$(dirname "$script")
 source ${script_path}/common.sh
+mysql_root_password=$1
+
+if [ -z "$mysql_root_password" ]; then
+  echo Input MySQL Root password missing
+  exit 1
+fi
 
 component=shipping
 schema_setup=mysql
-
 func_java
