69b518dc44ea0a7a2fdad1278130cc6319408bd6
meghasyam1997 <meghasyam01@gmail.com>
2025-02-22 13:04:42 +0530

    Mysql changes

--- mysql.sh
+++ mysql.sh
@@ -1,9 +1,22 @@
-echo -e "\e[35m>>>>>>>>>> Install MySQL Servere <<<<<<<<<<\e[0m"
-dnf install mysql-server -y
+script=$(realpath "$0")
+script_path=$(dirname "$script")
+source ${script_path}/common.sh
+mysql_root_password=$1
 
-echo -e "\e[35m>>>>>>>>>> Start MySQL Service <<<<<<<<<<\e[0m"
-systemctl enable mysqld
-systemctl start mysqld
+if [ -z "$mysql_root_password" ]; then
+  echo Input MySQL Root password missing
+  exit 1
+fi
 
-echo -e "\e[35m>>>>>>>>>> Change the default root password <<<<<<<<<<\e[0m"
-mysql_secure_installation --set-root-pass RoboShop@1
+print_head "Install MySQL Server"
+dnf install mysql-server -y &>>$log_file
+func_status_check $?
+
+print_head "Start MySQL Service"
+systemctl enable mysqld &>>$log_file
+systemctl start mysqld &>>$log_file
+func_status_check $?
+
+print_head "Change the default root password"
+mysql_secure_installation --set-root-pass $mysql_root_password &>>$log_file
+func_status_check $?
