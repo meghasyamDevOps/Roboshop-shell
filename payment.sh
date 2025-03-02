8689dd3c546741a78a037de258e58e7eefb97c4f
meghasyamdevops <meghasyamdevops@gmail.com>
2025-03-02 15:35:54 +0530

    make code dry

--- payment.sh
+++ payment.sh
@@ -1,6 +1,12 @@
 script=$(realpath "$0")
 script_path=$(dirname "$script")
 source ${script_path}/common.sh
+rabbitmq_appuser_password=$1
+
+if [ -z "$rabbitmq_appuser_password" ]; then
+  echo Input RabbitMQ App User password missing
+  exit 1
+fi
 
 component=payment
 
